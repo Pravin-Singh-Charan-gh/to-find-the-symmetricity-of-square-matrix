48e8b5220b38aac67d10f63c45f4cceae94fe1a6
Pravin-Singh-Charan-gh <125742995+Pravin-Singh-Charan-gh@users.noreply.github.com>
2023-03-19 17:12:28 +0530

    Update symmetric.c

--- symmetric.c
+++ symmetric.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 void main(){
-    int a,b;
+    int a;
     printf("Enter the dimension of square matrix:");
     scanf("%d",&a);
-    b=a;
-    int sq[a][b],i,j;
+    int sq[a][a],i,j;
     printf("Enter the elements of matrix row wise:");
     for(i=0;i<a;i++)
-    for(j=0;j<b;j++)
+    for(j=0;j<a;j++)
     scanf("%d",&sq[i][j]);
     for(i=0;i<a;i++)
-    for(j=0;j<b;j++){
+    for(j=0;j<a;j++){
         if(sq[i][j]!=sq[j][i]){
         printf("Matrix is not symmetric");
         goto nosym;}
-        else if(i==a-1 && j==b-1)
+        else if(i==a-1 && j==a-1)
         printf("Symmetric is matrix");
     }
     nosym:
